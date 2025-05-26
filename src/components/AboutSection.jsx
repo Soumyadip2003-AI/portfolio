c33988d16c022cb078104abebb9b4708e6bf4b93
AI-masterr <soumyadipsarkaruembca2025@gmail.com>
2025-05-27 04:25:24 +0530

    first commit

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,93 @@
+import { Briefcase, Code, User } from "lucide-react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Passionate Artificial Intelligence and Machine Learning Developer.
+            </h3>
+
+            <p className="text-muted-foreground">
+             I am a passionate and results-driven software developer with a strong foundation in Data Structures and Algorithms (DSA) and a growing expertise in Artificial Intelligence and Machine Learning (AI/ML). With a problem-solving mindset and a deep interest in computational thinking, I have honed my skills by solving hundreds of algorithmic challenges and competitive programming problems across various platforms.
+            </p>
+
+            <p className="text-muted-foreground">
+              I'm passionate about creating elegant solutions to complex
+              problems, and I'm constantly learning new technologies and
+              techniques to stay at the forefront of the ever-evolving web
+              landscape.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href="https://drive.google.com/file/d/14Ua6TmCYAiHj7rqfMLPb6VBn_4ESq576/view?usp=drivesdk"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> AIML Development</h4>
+                  <p className="text-muted-foreground">
+                    Creating advanced AI models and machine learning
+                    algorithms to solve real-world problems.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Data Structures and algorithms(DSA).</h4>
+                  <p className="text-muted-foreground">
+                    Proficient in DSA with a focus on problem-solving and
+                    algorithmic thinking.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Cloud Networking.</h4>
+                  <p className="text-muted-foreground">
+                    Experienced in cloud networking solutions, ensuring
+                    scalable and secure deployments.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,103 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+
+
+const projects = [
+  {
+    id: 1,
+    title: "stroke-prediction-system",
+    description: "Designed and implemented a predictive system leveraging AI and machine learning to assess stroke risk.Utilized data preprocessing and model optimization to ensure accurate predictions .",
+    image: "/projects/projects1.png",
+    tags: ["Python", "Pandas", "Scikit-learn", "Streamlit","Joblib","Shap"],
+    demoUrl: "https://stroke-prediction-system-ai-ml-j8mcfnxdyge3kpqkklb6ud.streamlit.app/",
+    githubUrl: "https://github.com/Soumyadip2003-AI/stroke-prediction-system-ai-ml.git",
+  },
+  {
+    id: 2,
+    title: "Mental Health Crisis Detector",
+    description:
+      "Developed a comprehensive mental health crisis detection system using advanced NLP techniques and machine learning algorithms. The system analyzes user inputs to identify potential mental health crises, providing timely alerts and resources.",
+    image: "/projects/projects2.png",
+    tags: ["Python", "Pandas", "NumPy", "Streamlit", "scikit-learn", "Joblib", "Shap", "Matplotlib", "Seaborn", "nltk","transformers","spacy","gensim","opencv"],
+    demoUrl: "https://cmrbyzcsb74mtrlbcsfplb.streamlit.app/",
+    githubUrl: "#",
+  },
+ 
+];
+
+export const ProjectsSection = () => {
+  return (
+    <section id="projects" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          {" "}
+          Featured <span className="text-primary"> Projects </span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Each project was carefully
+          crafted with attention to detail, performance, and user experience.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" >
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="p-6">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag) => (
+                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+
+                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a
+                      href={project.demoUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <ExternalLink size={20} />
+                    </a>
+                    <a
+                      href={project.githubUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <Github size={20} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+            target="_blank"
+            href="https://github.com/Soumyadip2003-AI"
+          >
+            Check My Github <ArrowRight size={16} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
